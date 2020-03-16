f8092812d742a84560d68dd0ba14e161c558899a
Anirudh Dagar <anirudhdagar6@gmail.com>
2020-03-16 10:16:24 +0530

    [Add] shell run scripts

---
+++ get_karate_club.sh
@@ -0,0 +1,6 @@
+curl -OL http://konect.cc/files/download.tsv.ucidata-zachary.tar.bz2
+tar xvzf download.tsv.ucidata-zachary.tar.bz2
+rm download.tsv.ucidata-zachary.tar.bz2
+mv ucidata-zachary resources
+
+echo "Successfully Downloaded the Karate Club Dataset"
---
+++ run.sh
@@ -0,0 +1,4 @@
+python train.py
+g++ main.cpp -I eigen -std=c++17
+./a.out
+
